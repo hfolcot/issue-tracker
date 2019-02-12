0f4e75d16674c83da0342ece2770f1a90071ebd9
Heather <hfolcot@gmail.com>
2019-02-12 14:47:25 +0000

    added js function to store active tabs in the window

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,37 @@
 $(document).ready(function() {
+
+	//Render markdown where called
 	$(".article-content").each(function() {
 		var article = $(this).text();
 		var marked_article = marked(article)
 		$(this).html(marked_article)
 	});
+
+	//Store which tab is currently selected, in the session window
+	$('.nav-link').click(function(){
+   	sessionStorage.setItem("clickedTab", $(this).attr('id'));
+   	sessionStorage.setItem("activePane", $(this).attr('href'));
+	});
+
+	if(typeof sessionStorage.getItem("activePane") != null && sessionStorage.getItem("clickedTab") != "undefined") {
+		$("#feature-results").removeClass("show active");
+		console.log("removed class from feature-results")
+		$('#featuresTab').removeClass("active");
+		console.log("removed class from featuresTab")
+		console.log(sessionStorage.getItem("clickedTab"));
+		activeTabId = "#" + sessionStorage.getItem("clickedTab")
+		console.log(activeTabId);
+		activePaneId = sessionStorage.getItem("activePane")
+		console.log(activePaneId)
+    	$(activeTabId).addClass("active");
+    	console.log("added class to active tab");
+    	$(activePaneId).addClass("show active");
+    	console.log("added class to active pane")
+
+	} else {
+		$("#feature-results").addClass("show active");
+		$('#featuresTab').addClass("active");
+		console.log("no item to get")
+	}
 
 })
