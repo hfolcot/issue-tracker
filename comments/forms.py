e262b17a53fb864f752f80c1b7670bf0ed75a26a
Heather <hfolcot@gmail.com>
2019-03-26 14:34:19 +0000

    removed required false attr from comment form

--- comments/forms.py
+++ comments/forms.py
@@ -4,4 +4,4 @@ from .models import Comment
 class CommentForm(forms.Form):
 	content_type = forms.CharField(widget=forms.HiddenInput)
 	object_id = forms.IntegerField(widget=forms.HiddenInput)
-	content = forms.CharField(widget=forms.Textarea(attrs={'rows': '5'}), label='', required=False)
+	content = forms.CharField(widget=forms.Textarea(attrs={'rows': '5'}), label='')
